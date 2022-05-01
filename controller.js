d43b2a80b171c229b97fbdf853f856222d1758e6
richard <wiryadinatarichard1@gmail.com>
2022-05-01 15:22:17 +0700

    melakukan update data mahasiswa berdasarkan id

--- controller.js
+++ controller.js
@@ -41,11 +41,28 @@ exports.tambahMahasiswa = function (req, res) {
 
     connection.query('INSERT INTO mahasiswa (nim,nama,jurusan) VALUES (?,?,?)',
         [nim, nama, jurusan], function (error, rows, fields) {
-            if(error){
+            if (error) {
                 console.log(error);
-            }else{
-                response.ok("Berhasil Menambahkan Data", res)
+            } else {
+                response.ok("Berhasil Menambahkan Data", res);
             }
         });
 
+};
+
+//mengupdate data berdasarkan id
+exports.ubahMahasiswa = function (req, res) {
+    let id = req.body.id_mahasiswa;
+    const nim = req.body.nim;
+    const nama = req.body.nama;
+    const jurusan = req.body.jurusan;
+
+    connection.query('UPDATE mahasiswa SET nim=?, nama=?, jurusan=? WHERE id_mahasiswa=?',
+        [nim, nama, jurusan, id], function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok("Berhasil Mengubah Data", res);
+            }
+        });
 };
