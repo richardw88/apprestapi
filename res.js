f38f5c4c72443f2d7300628c0020eabf0994909f
richard <wiryadinatarichard1@gmail.com>
2022-05-02 22:03:51 +0700

    membuat tampil matakuliah

--- res.js
+++ res.js
@@ -10,3 +10,30 @@ exports.ok = function(values, res){
      res.end();
 }
 
+//response untuk nested matakuliah
+exports.oknested = function(values,res) {
+    //lakukan akumulasi, reduce untuk membuat json menjadi nested
+    const hasil = values.reduce((akumulasi, item)=>{
+        //tentukan key group
+        if(akumulasi[item.nama]){
+            //buat variable group nama mahasiswa
+            const group = akumulasi[item.nama];
+            //cek jika isi array adalah matakuliah
+            if(Array.isArray(group.matakuliah)){
+                //tambahkan value ke dalam group matakuliah
+                group.matakuliah.push(item.matakuliah);
+            }else{
+                group.matakuliah = [group.matakuliah, item.matakuliah];
+            }
+        }else{
+            akumulasi[item.nama] = item;
+        }
+        return akumulasi;
+    },{});
+    var data = {
+        'status' : 200,
+        'values' :  hasil
+    };
+    res.json(data);
+    res.end();
+}
